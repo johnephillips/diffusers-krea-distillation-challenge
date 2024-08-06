8b736e5be4b68fbc11c4d2376a7a9ad6579a93e0
Ubuntu <ubuntu@dev-3.us-northcentral1-a.compute.internal>
2024-08-06 01:51:26 +0000

    Get train.sh working with saving checkpoints on /data

--- examples/text_to_image/train.sh
+++ examples/text_to_image/train.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export CHECKPOINT_DIR="/data/john-challenge"
 export MODEL_NAME="stabilityai/stable-diffusion-xl-base-1.0"
 export VAE_NAME="madebyollin/sdxl-vae-fp16-fix"
 export DATASET_NAME="lambdalabs/naruto-blip-captions"
@@ -16,13 +17,16 @@ accelerate launch train_text_to_image_sdxl.py \
   --random_flip \
   --proportion_empty_prompts=0.2 \
   --train_batch_size=1 \
-  --max_train_steps=10000 \
+  --max_train_steps=2 \
   --learning_rate=1e-06 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
   --mixed_precision="fp16" \
   --validation_prompt="a cute Sundar Pichai creature" \
-  --validation_epochs 5 \
-  --checkpointing_steps=5000 \
-  --output_dir="sdxl-naruto-model-$timestamp" \
+  --validation_epochs 1 \
+  --checkpointing_steps=1 \
+  --output_dir="$CHECKPOINT_DIR/sdxl-naruto-model-$timestamp" \
   --report_to="wandb" \
+  #--max_train_steps=10000 \
+  #--checkpointing_steps=5000 \
+  #--validation_epochs 5 \
